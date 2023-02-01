e0ba85cc0d666094b763dbcac444b238dff3d7a5
Jay Frey <jf.kuan@omesti.com>
2023-02-01 12:19:38 +0800

    Setup docker and healthcheck for API app

---
+++ api/healthcheck.js
@@ -0,0 +1,25 @@
+const http = require("http");
+
+// import http from "http";
+const options = {
+  host: "0.0.0.0",
+  port: 3000,
+  timeout: 2000,
+  path: "/healthcheck",
+};
+
+const healthCheck = http.request(options, (res) => {
+  console.log(`HEALTHCHECK STATUS: ${res.statusCode}`);
+  if (res.statusCode == 200) {
+    process.exit(0);
+  } else {
+    process.exit(1);
+  }
+});
+
+healthCheck.on("error", function (err) {
+  console.error("ERROR");
+  process.exit(1);
+});
+
+healthCheck.end();
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,5 +1,9 @@
 import { AppController } from "./controllers/AppController";
+import { StatusCodes } from "http-status-codes";
 
 export default (app: any) => {
+  app.get("/healthcheck", (req: any, res: any) => {
+    res.sendStatus(StatusCodes.OK);
+  });
   app.get("/car-park/availability", new AppController().getCarParkAvailability);
 };
